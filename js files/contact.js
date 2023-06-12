232674c14e1949ca280f79cf80340def61590da0
gilbert-ku <gilbert45ku@gmail.com>
2023-06-12 22:12:10 +0300

    add javascript file to contacts get elemwnt by id

---
+++ js files/contact.js
@@ -0,0 +1,8 @@
+const userName = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let massgeInput = document.getElementById("massage");
+let checkbox = document.getElementById("checkbox");
+let button = document.getElementById("button");
+
+
+
